a7ce19ebcebb2e1bc2ef5a02e0629c11e0ad93f0
Alana Wesly <awesly@hawaii.edu>
2025-05-03 17:05:34 -1000

    issue-48: added admin functionality

--- src/components/SightingCard.tsx
+++ src/components/SightingCard.tsx
@@ -4,33 +4,40 @@ import { Card, Image } from 'react-bootstrap';
 import Link from 'next/link';
 import { Sighting } from '@prisma/client';
 
-/* Renders a single Sighting. See list-birds/page.tsx and list-all-sightings/page.tsx for usage.*/
+/* Renders a single Sighting. See list-birds/page.tsx and list-all-sightings/page.tsx for usage. 
+  Added current user's role for edit / delete permissions */
 const SightingCard = ({
   sighting,
   currentUserEmail,
+  currentUserRole,
 }: {
   sighting: Sighting;
   currentUserEmail: string | null;
-}) => (
-  <Card className="h-100">
-    <Card.Header>
-      <Image src={sighting.imagepath ?? 'public/bird-00.jpg'} alt={sighting.name} width={75} />
-      <Card.Title>{sighting.name}</Card.Title>
-      <Card.Subtitle>{sighting.time}</Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text>{sighting.description}</Card.Text>
-    </Card.Body>
-    { /*  updated to only show edit and delete for the user's own sightings. */}
-    <Card.Footer>
-      {currentUserEmail === sighting.owner && (
-        <>
-          <Link href={`edit/${sighting.id}`} className="me-3">Edit</Link>
-          <Link href={`delete/${sighting.id}`} className="text-danger">Delete</Link>
-        </>
-      )}
-    </Card.Footer>
-  </Card>
-);
+  currentUserRole: string | null;
+}) => {
+  const canEditOrDelete =
+    currentUserEmail === sighting.owner || currentUserRole === 'ADMIN';
+
+  return (
+    <Card className="h-100">
+      <Card.Header>
+        <Image src={sighting.imagepath ?? 'public/bird-00.jpg'} alt={sighting.name} width={75} />
+        <Card.Title>{sighting.name}</Card.Title>
+        <Card.Subtitle>{sighting.time}</Card.Subtitle>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>{sighting.description}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        {canEditOrDelete && (
+          <>
+            <Link href={`edit/${sighting.id}`} className="me-3">Edit</Link>
+            <Link href={`delete/${sighting.id}`} className="text-danger">Delete</Link>
+          </>
+        )}
+      </Card.Footer>
+    </Card>
+  );
+};
 
 export default SightingCard;
