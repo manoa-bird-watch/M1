82ba566915b2940b16c5d6db677a15096adccc9e
Jeffrey Jian <jeffreyj8193@gmail.com>
2025-05-06 10:39:38 -1000

    Fixed navigation errors

--- src/components/NavbarTemp.tsx
+++ src/components/NavbarTemp.tsx
@@ -10,8 +10,8 @@ import { BoxArrowRight, Lock, PersonFill, PersonPlusFill } from 'react-bootstrap
 const NavbarTemp: React.FC = () => {
   const { data: session } = useSession();
   const currentUser = session?.user?.email;
-  const userWithRole = session?.user as { email: string; randomKey: string };
-  const role = userWithRole?.randomKey;
+  const userWithRole = session?.user as { email: string; role: string };
+  const role = userWithRole?.role;
   const pathName = usePathname();
   return (
     <Navbar bg="light" expand="lg">
